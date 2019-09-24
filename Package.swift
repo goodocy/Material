2c81c1d705a8eba1d49be49f1c6b6da8afcc8d01
Jaeyun,Oh <goodocy@linecorp.com>
2019-09-24 14:03:09 +0900

    Test

--- Package.swift
+++ Package.swift
@@ -1,9 +1,27 @@
 // swift-tools-version:4.2
 import PackageDescription
 
-let package = Package(
+let iOS_package = Package(
     name: "Material",
-    // platforms: [.iOS("8.0")],
+    platforms: [.iOS("8.0")],
+    products: [
+        .library(name: "Material", targets: ["Material"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/CosmicMind/Motion.git", .upToNextMajor(from: "3.1.0")),
+    ],
+    targets: [
+        .target(
+            name: "Material",
+            dependencies: ["Motion"],
+            path: "Sources"
+        )
+    ]
+)
+
+let mac_package = Package(
+    name: "Material",
+    platform: [.mac("10.10)"],
     products: [
         .library(name: "Material", targets: ["Material"])
     ],
